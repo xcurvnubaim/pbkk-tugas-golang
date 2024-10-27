f565265a4c5ffdee1726470ac5552ccfe5135c4c
xcurvnubaim <zhariffmarzuqi@gmail.com>
2024-10-28 06:03:22 +0700

    fix: panic on input not a number

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,14 +1,23 @@
 package main
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 func main() {
 	if(len(os.Args) < 2) {
 		println("Usage : main <number>")
 		return
-	}	
+	}
 
-	if os.Args[1] == "42" {
+	i, err := strconv.Atoi(os.Args[1]); 
+	
+	if err != nil {
+		panic("input must be a number")
+	} 
+
+	if i == 42 {
 		println("Hello, Universe!")
 	} else {
 		println(os.Args[1])
